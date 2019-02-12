13d5e12580f0c3da68a99f46ab88c42e3ec2b343
Philipp Uhl <git@ph-uhl.com>
2019-02-12 18:25:28 +0100

    Removes server components from this repo

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 import { Login } from './client/screens/Signup';
 import { Signup } from './client/screens/Login';
 
-import Routes from './server/routes/user';
-import Model from './server/model/user';
-import { Errors } from './server/util/errors';
-
-const Client = {
-  Login, Signup
-};
-
-const Server = {
-  Routes: { User: Routes }, Models: { User: Model }, Errors
-};
-
-export { Client, Server };
+export { Login, Signup };
